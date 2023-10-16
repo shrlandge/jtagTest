eab77fd5e5af9d3ce35aee20868e42f5b2d122a8
Piao DENG <85469000+pauloohaha@users.noreply.github.com>
2023-10-16 12:34:28 -0700

    debug

--- l2_ram_multi_bank.sv
+++ l2_ram_multi_bank.sv
@@ -8,7 +8,27 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-`include "soc_mem_map.svh"
+
+`define SOC_MEM_MAP_TCDM_START_ADDR          32'h1C01_0000
+`define SOC_MEM_MAP_TCDM_END_ADDR            32'h1C09_0000
+`define SOC_MEM_MAP_TCDM_ALIAS_START_ADDR    32'h0000_0000
+`define SOC_MEM_MAP_TCDM_ALIAS_END_ADDR      32'h0010_0000
+
+`define SOC_MEM_MAP_PRIVATE_BANK0_START_ADDR 32'h1C00_0000
+`define SOC_MEM_MAP_PRIVATE_BANK0_END_ADDR   32'h1C00_8000
+
+`define SOC_MEM_MAP_PRIVATE_BANK1_START_ADDR 32'h1C00_8000
+`define SOC_MEM_MAP_PRIVATE_BANK1_END_ADDR   32'h1C01_0000
+
+`define SOC_MEM_MAP_BOOT_ROM_START_ADDR      32'h1A00_0000
+`define SOC_MEM_MAP_BOOT_ROM_END_ADDR        32'h1A04_0000
+
+`define SOC_MEM_MAP_AXI_PLUG_START_ADDR      32'h1000_0000
+`define SOC_MEM_MAP_AXI_PLUG_END_ADDR        32'h1040_0000
+
+`define SOC_MEM_MAP_PERIPHERALS_START_ADDR   32'h1A10_0000
+`define SOC_MEM_MAP_PERIPHERALS_END_ADDR     32'h1A40_0000
+
 
 module l2_ram_multi_bank #(
    parameter NB_BANKS                   = 4,
